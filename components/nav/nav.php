02cc097f264de0cedad0935fa21e50637aa05a32
Brendan Hytrek <bjhytrek@gmail.com>
2016-01-18 14:49:31 -0700

    building default style with vars and mixins from CHP.

---
+++ components/nav/nav.php
@@ -0,0 +1,6 @@
+<nav class="nav" id="site-nav">
+    <ul class="list">
+        <li><a href="#">CIT336</a></li>
+        <li><a href="#">CS313</a></li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <!doctype html>
 
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <title>Brendan Hytrek</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
+    <title>Brendan Hytrek</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
 
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
-  <h1>Brendan Hytrek</h1>
-  <h2>Welcome
-  </h2>
+   <?php @include 'components/header/header.php' ?>
+    <main class="bh">
+       
+
+        <h1>Brendan Hytrek</h1>
+        <h2>Welcome</h2>
+
+    </main>
+
 </body>
+
 </html>
