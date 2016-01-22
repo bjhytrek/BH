c62d4bd532150ad7c730f4873e6e0392fc1ea51c
Brendan Hytrek <bjhytrek@gmail.com>
2016-01-22 01:34:14 -0700

    changing URL

--- pages/cs313.php
+++ pages/cs313.php
@@ -1,5 +1,5 @@
 <?php $_POST["site_id"] = "cit313";  ?>
 <h2>CS313 Assignments</h2>
 <ul>
-    <li class="flow-text"><a href="/BH/assignments/php_survey/survey.php">Php Survey</a></li>
+    <li class="flow-text"><a href="/assignments/php_survey/survey.php">Php Survey</a></li>
 </ul>
