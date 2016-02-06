a0210e45d0455dd7d61f92c2c5f5d0af9c65afd8
Brendan Hytrek <bjhytrek@gmail.com>
2016-02-05 19:47:46 -0700

    setting up remote credentials for exercise04

--- assignments/cit336/exercises/chapter04/database.php
+++ assignments/cit336/exercises/chapter04/database.php
@@ -3,11 +3,38 @@
     $username = 'mgs_user';
     $password = 'pa55word';
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('database_error.php');
-        exit();
-    }
+        if ($openShiftVar === null || $openShiftVar == "")
+     {
+          // Not in the openshift environment
+//          echo "Using local credentials: "; 
+//         echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+            try {
+                $db = new PDO($dsn, $username, $password);
+                } catch (PDOException $e) {
+                    $error_message = $e->getMessage();
+                    include('database_error.php');
+                    exit();
+                }
+     }
+     else 
+     { 
+          // In the openshift environment
+//          echo "Using openshift credentials: ";
+
+          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+         try {
+                $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo 'Connection failed: '. $e->getMessage();
+                }
+     } 
+     
+    
+
+
+    
 ?>
