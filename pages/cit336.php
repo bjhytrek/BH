61d9ae019c5e46d63ba21fa802e6cc56debf16c4
Brendan Hytrek <bjhytrek@gmail.com>
2016-02-08 11:39:43 -0700

    cit336 exercise last changes.

--- pages/cit336.php
+++ pages/cit336.php
@@ -3,6 +3,5 @@
 <ul>
     <li class="flow-text"><a href="/assignments/cit336/exercises/chapter02/index.html">Chapter 2 Exercise 1</a></li>
     <li class="flow-text"><a href="/assignments/cit336/enhancements/chapter02/index.html">Chapter 2 Enhancement</a></li>
-    <li class="flow-text"><a href="/assignments/cit336/exercises/chapter04/index.php">Chapter 4 Exercise 1</a></li>
-    <li class="flow-text"><a href="/assignments/cit336/enhancements/chapter04/index.php">Chapter 4 Enhancement</a></li>
+    <li class="flow-text"><a href="/assignments/cit336/exercises/chapter04/index.php">Chapter 4 Exercise/Enhancement 1</a></li>
 </ul>
