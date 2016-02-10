4f493e321bf8e9d078b7b764a8b303dd41591ba4
Brendan Hytrek <bjhytrek@gmail.com>
2016-02-10 11:11:16 -0700

    pushing to open shift to test if production site can access amazon via the javaScript SDK

--- pages/amazon.php
+++ pages/amazon.php
@@ -1,25 +1,65 @@
 <?php $_POST["site_id"] = "amazon";  ?>
 
     <h1>Log in and Pay with Amazon</h1>
-    <div id="AmazonLoginButton"></div>
 
-    <div id="AmazonPayButton" />
+    <div id="amazon-root"></div>
     <script type="text/javascript">
+        window.onAmazonLoginReady = function () {
+            amazon.Login.setClientId('amzn1.application-oa2-client.ef152a240a7741718b01300dc81d08b8');
+        };
+        (function (d) {
+            var a = d.createElement('script');
+            a.type = 'text/javascript';
+            a.async = true;
+            a.id = 'amazon-login-sdk';
+            a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
+            d.getElementById('amazon-root').appendChild(a);
+        })(document);
+    </script>
+    <!--
+    <div class="text-center" style="margin-top:40px;" id="AmazonPayButton"></div>
+    <script type='text/javascript'>
+        window.onAmazonLoginReady = function () {
+            amazon.Login.setClientId('amzn1.application-oa2-client.ef152a240a7741718b01300dc81d08b8');
+            amazon.Login.setUseCookie(true);
+        };
+    </script>
+-->
+    <a href id="LoginWithAmazon">
+        <img border="0" alt="Login with Amazon" src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/
+btnLWA_gold_156x32.png" width="156" height="32" />
+    </a>
+    <a id="Logout">Logout</a>
+
+    <script type="text/javascript">
+        document.getElementById('LoginWithAmazon').onclick = function () {
+            options = {
+                scope: 'profile'
+            };
+            amazon.Login.authorize(options,
+                'https://index.php?page=handle_login');
+            return false;
+        };
+    </script>
+    
+    
+    
+    
+    
+    <script type='text/javascript' src='https://static-na.payments-amazon.com/OffAmazonPayments/us/sandbox/js/Widgets.js'></script>
+    <script type='text/javascript'>
         var authRequest;
         OffAmazonPayments.Button("AmazonPayButton", "amzn1.application-oa2-client.ef152a240a7741718b01300dc81d08b8", {
-            type: "LwA",
-            color: "Gold",
-            size: "small",
+            type: "PwA",
             authorization: function () {
                 loginOptions = {
-                    scope: "payments:widget payments:shipping_address",
-                    popup: true;
+                    scope: "profile postal_code payments:widget payments:shipping_address",
+                    popup: true
                 };
-                authRequest = amazon.Login.authorize(loginOptions,
-                    "YOUR_REDIRECT_URL_HERE");
+                authRequest = amazon.Login.authorize(loginOptions, "/index.php?page=amazon");
             },
             onError: function (error) {
-                // your error handling code
+                // something bad happened
             }
         });
     </script>
---
+++ pages/handle_login.php
@@ -0,0 +1,27 @@
+<?php
+// Verify that the access token belongs to us
+// The token must be url-encoded when passed to tokeninfo
+$c = curl_init('https://api.amazon.com/auth/o2/tokeninfo?access_token='
+. urlencode($_REQUEST['access_token']));
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+$r = curl_exec($c);
+curl_close($c);
+$d = json_decode($r);
+if ($d->aud != 'amzn1.application-oa2-client.ef152a240a7741718b01300dc81d08b8') {
+// the access token does not belong to us
+header('HTTP/1.1 404 Not Found');
+echo 'Page not found';
+exit;
+}
+// Exchange the access token for user profile
+// The token must NOT be url-encoded when passed to profile
+$c = curl_init('https://api.amazon.com/user/profile');
+curl_setopt($c, CURLOPT_HTTPHEADER, array('Authorization: bearer '
+. $_REQUEST['access_token']));
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+$r = curl_exec($c);
+curl_close($c);
+$d = json_decode($r);
+echo sprintf('%s %s %s', $d->name, $d->email, $d->user_id);
+
+?>
