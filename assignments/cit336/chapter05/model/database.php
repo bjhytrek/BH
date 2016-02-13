a097aeeb89f150e0b9d1fd0b0fcb0eef89803a5c
Brendan Hytrek <bjhytrek@gmail.com>
2016-02-13 13:31:14 -0700

    changed my database username and password to troubleshoot a db connection problem in open shift

--- assignments/cit336/chapter05/model/database.php
+++ assignments/cit336/chapter05/model/database.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $username = 'bjhytrek';
+    $password = 'Drpepper1991';
 
     try {
         $db = new PDO($dsn, $username, $password);
