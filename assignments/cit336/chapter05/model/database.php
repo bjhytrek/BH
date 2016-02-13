70cf861ec646a7be66255821a1e4d07c50c4c394
Brendan Hytrek <bjhytrek@gmail.com>
2016-02-13 13:53:18 -0700

    Remembered that open shift needs special environment variables to be able to connect to the database. Pushing those up.

--- assignments/cit336/chapter05/model/database.php
+++ assignments/cit336/chapter05/model/database.php
@@ -1,13 +1,42 @@
 <?php
     $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'bjhytrek';
-    $password = 'Drpepper1991';
+    $username = 'mgs_user';
+    $password = 'pa55word';
+    $dbName = "my_guitar_shop1";
+    $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('../errors/database_error.php');
-        exit();
-    }
+
+        if ($openShiftVar === null || $openShiftVar == "")
+     {
+          // Not in the openshift environment
+//          echo "Using local credentials: "; 
+//         echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+            try {
+                $db = new PDO($dsn, $username, $password);
+                } catch (PDOException $e) {
+                    $error_message = $e->getMessage();
+                    include('database_error.php');
+                    exit();
+                }
+     }
+     else 
+     { 
+          // In the openshift environment
+//          echo "Using openshift credentials: ";
+
+          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+         try {
+                $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                $error_message = $e->getMessage();
+                include('../errors/database_error.php');
+                exit();
+                }
+     } 
+     
+    
 ?>
